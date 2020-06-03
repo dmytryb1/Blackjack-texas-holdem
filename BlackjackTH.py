6c7861ddda16affb8b3c64421d5bf793e571a037
Dmytry <dmytry.lb1@gmail.com>
2020-06-03 22:38:19 +0500

    Update BlackjackTH.py

--- BlackjackTH.py
+++ BlackjackTH.py
@@ -1,5 +1,13 @@
 import random
 
+value_card_one = 0
+value_card_two = 0
+value_card_three = 0
+value_card_four = 0
+value_card_five = 0
+value_card_six = 0
+value_card_seven = 0
+
 # Base deck of cards
 cards = {'h1': 1, 'h2': 2, 'h3': 3, 'h4': 4, 'h5': 5, 'h6': 6, 'h7': 7, 'h8': 8, 'h9': 9, 'h10': 10,
          'hj': 10, 'hq': 10, 'hk': 10,
@@ -9,113 +17,140 @@ cards = {'h1': 1, 'h2': 2, 'h3': 3, 'h4': 4, 'h5': 5, 'h6': 6, 'h7': 7, 'h8': 8,
          'sj': 10, 'sq': 10, 'sk': 10,
          'c1': 1, 'c2': 2, 'c3': 3, 'c4': 4, 'c5': 5, 'c6': 6, 'c7': 7, 'c8': 8, 'c9': 9, 'c10': 10,
          'cj': 10, 'cq': 10, 'ck': 10}
-# dealth card
-#dealtcard = random.choice(list(cards))
-#print(dealtcard)
 
-# remove dealt card from deck
-#cards.pop(dealtcard)
-#print(cards)
+
 
 # Turns
 playing = True
 while playing == True:
-    card1 = random.choice(list(cards))
-    valcard1 = cards[card1]
-    if valcard1 == 1:
-        valcard1 = 11
-    cards.pop(card1)
-    card2 = random.choice(list(cards))
-    valcard2 = cards[card2]
-    if valcard2 == 1 and valcard1 != 11:
-        valcard2 = 11
-    cards.pop(card2)
-    print(card1 + ' ' + card2)
-    comphand = valcard1 + valcard2
-    print(comphand)
-    if comphand == 21:
+    card_one = random.choice(list(cards))
+    value_card_one = cards[card_one]
+    if value_card_one == 1:
+        value_card_one = 11
+    cards.pop(card_one)
+    card_two = random.choice(list(cards))
+    value_card_two = cards[card_two]
+    if value_card_two == 1 and value_card_one != 11:
+        value_card_two = 11
+    cards.pop(card_two)
+    print(card_one + ' ' + card_two)
+    computer_hand = value_card_one + value_card_two
+    print(computer_hand)
+    if computer_hand == 21:
         print("Computer got Blackjack!")
         playing = False
-    if 17 <= comphand <= 21:
+        break
+    if 17 <= computer_hand <= 21:
         print('stay')
         playing = False
-    if comphand <= 16:
-        card3 = random.choice(list(cards))
-        valcard3 = cards[card3]
-        cards.pop(card3)
-        comphand = valcard1 + valcard2 + valcard3
-        print(card1 + ' ' + card2 + ' ' + card3)
-        print(comphand)
-        if comphand > 21 and valcard1 == 11:
-            valcard1 == 1
-            comphand = valcard1 + valcard2 + valcard3
-        if comphand > 21 and valcard2 == 11:
-            valcard2 == 1
-            comphand = valcard1 + valcard2 + valcard3
-        if comphand > 21:
+    if computer_hand <= 16:
+        card_three = random.choice(list(cards))
+        value_card_three = cards[card_three]
+        cards.pop(card_three)
+        if value_card_three == 1:
+            value_card_three = 11
+        computer_hand = value_card_one + value_card_two + value_card_three
+        print(card_one + ' ' + card_two + ' ' + card_three)
+        if computer_hand > 21 and value_card_one == 11:
+            value_card_one = 1
+            computer_hand = value_card_one + value_card_two + value_card_three
+        if computer_hand > 21 and value_card_two == 11:
+            value_card_two = 1
+            computer_hand = value_card_one + value_card_two + value_card_three
+        if computer_hand > 21 and value_card_three == 11:
+            value_card_three = 1
+            computer_hand = value_card_one + value_card_two + value_card_three + value_card_four
+        if computer_hand > 21 and value_card_four == 11:
+            value_card_four = 1
+            computer_hand = value_card_one + value_card_two + value_card_three + value_card_four + value_card_five
+        print(computer_hand)
+        if computer_hand > 21:
             print("You Bust")
             playing = False
-        if comphand >= 17 and comphand <= 21:
+        if 17 <= computer_hand <= 21:
             print('stay')
             playing = False
-        if comphand <= 16:
-            card4 = random.choice(list(cards))
-            valcard4 = cards[card4]
-            cards.pop(card4)
-            comphand = valcard1 + valcard2 + valcard3 + valcard4
-            print(card1 + ' ' + card2 + ' ' + card3 + ' ' + card4)
-            print(comphand)
-            if comphand > 21 and valcard1 == 11:
-                valcard1 == 1
-                comphand = valcard1 + valcard2 + valcard3 + valcard4
-            if comphand > 21 and valcard2 == 11:
-                valcard2 == 1
-                comphand = valcard1 + valcard2 + valcard3 + valcard4
-            if comphand > 21:
+        if computer_hand <= 16:
+            card_four = random.choice(list(cards))
+            value_card_four = cards[card_four]
+            cards.pop(card_four)
+            if value_card_four == 1:
+                value_card_four = 11
+            computer_hand = value_card_one + value_card_two + value_card_three + value_card_four
+            print(card_one + ' ' + card_two + ' ' + card_three + ' ' + card_four)
+            if computer_hand > 21 and value_card_one == 11:
+                value_card_one = 1
+                computer_hand = value_card_one + value_card_two + value_card_three
+            if computer_hand > 21 and value_card_two == 11:
+                value_card_two = 1
+                computer_hand = value_card_one + value_card_two + value_card_three
+            if computer_hand > 21 and value_card_three == 11:
+                value_card_three = 1
+                computer_hand = value_card_one + value_card_two + value_card_three + value_card_four
+            if computer_hand > 21 and value_card_four == 11:
+                value_card_four = 1
+                computer_hand = value_card_one + value_card_two + value_card_three + value_card_four + value_card_five
+            print(computer_hand)
+            if computer_hand > 21:
                 print("You Bust")
                 playing = False
-            if comphand >= 17 and comphand <= 21:
+            if 17 <= computer_hand <= 21:
                 print('stay')
                 playing = False
-            if comphand <= 16:
-                card5 = random.choice(list(cards))
-                valcard5 = cards[card5]
-                cards.pop(card5)
-                comphand = valcard1 + valcard2 + valcard3 + valcard4 + valcard5
-                print(card1 + ' ' + card2 + ' ' + card3 + ' ' + card4 + ' ' + card5)
-                print(comphand)
-                if comphand > 21 and valcard1 == 11:
-                    valcard1 == 1
-                    comphand = valcard1 + valcard2 + valcard3 + valcard5
-                if comphand > 21 and valcard2 == 11:
-                    valcard2 == 1
-                    comphand = valcard1 + valcard2 + valcard3 + valcard5
-                if comphand > 21:
+            if computer_hand <= 16:
+                card_five = random.choice(list(cards))
+                value_card_five = cards[card_five]
+                cards.pop(card_five)
+                if value_card_five == 1:
+                    value_card_five = 11
+                computer_hand = value_card_one + value_card_two + value_card_three + value_card_four + value_card_five
+                print(card_one + ' ' + card_two + ' ' + card_three + ' ' + card_four + ' ' + card_five)
+                if computer_hand > 21 and value_card_one == 11:
+                    value_card_one = 1
+                    computer_hand = value_card_one + value_card_two + value_card_three
+                if computer_hand > 21 and value_card_two == 11:
+                    value_card_two = 1
+                    computer_hand = value_card_one + value_card_two + value_card_three
+                if computer_hand > 21 and value_card_three == 11:
+                    value_card_three = 1
+                    computer_hand = value_card_one + value_card_two + value_card_three + value_card_four
+                if computer_hand > 21 and value_card_four == 11:
+                    value_card_four = 1
+                    computer_hand = value_card_one + value_card_two + value_card_three + value_card_four + value_card_five
+                print(computer_hand)
+                if computer_hand > 21:
                     print("You Bust")
                     playing = False
-                if comphand >= 17 and comphand <= 21:
+                if 17 <= computer_hand <= 21:
                     print('stay')
                     playing = False
-                if comphand <= 16:
-                    card6 = random.choice(list(cards))
-                    valcard6 = cards[card6]
-                    cards.pop(card6)
-                    comphand = valcard1 + valcard2 + valcard3 + valcard4 + valcard5 + valcard6
-                    print(card1 + ' ' + card2 + ' ' + card3 + ' ' + card4 + ' ' + card5 + ' ' + card6)
-                    print(comphand)
-                    if comphand > 21 and valcard1 == 11:
-                        valcard1 == 1
-                        comphand = valcard1 + valcard2 + valcard3 + valcard6
-                    if comphand > 21 and valcard2 == 11:
-                        valcard2 == 1
-                        comphand = valcard1 + valcard2 + valcard3 + valcard6
-                    if comphand > 21:
+                if computer_hand <= 16:
+                    card_six = random.choice(list(cards))
+                    value_card_six = cards[card_six]
+                    cards.pop(card_six)
+                    if value_card_six == 1:
+                        value_card_six = 11
+                    computer_hand = value_card_one + value_card_two + value_card_three + value_card_four + value_card_five + value_card_six
+                    print(card_one + ' ' + card_two + ' ' + card_three + ' ' + card_four + ' ' + card_five + ' ' + card_six)
+                    if computer_hand > 21 and value_card_one == 11:
+                        value_card_one = 1
+                        computer_hand = value_card_one + value_card_two + value_card_three
+                    if computer_hand > 21 and value_card_two == 11:
+                        value_card_two = 1
+                        computer_hand = value_card_one + value_card_two + value_card_three
+                    if computer_hand > 21 and value_card_three == 11:
+                        value_card_three = 1
+                        computer_hand = value_card_one + value_card_two + value_card_three + value_card_four
+                    if computer_hand > 21 and value_card_four == 11:
+                        value_card_four = 1
+                        computer_hand = value_card_one + value_card_two + value_card_three + value_card_four + value_card_five
+                        print(computer_hand)
+                    if computer_hand > 21:
                         print("You Bust")
                         playing = False
-                    if comphand >= 17 and comphand <= 21:
+                    if 17 <= computer_hand <= 21:
                         print('stay')
                         playing = False
                     else:
                         print("You Bust")
                         playing = False
-
